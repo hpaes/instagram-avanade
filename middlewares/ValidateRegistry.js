2c571e4d3a88d8fe8283cdc6c46428e6a5ae0120
Herbert Paes <herbert_pbarreto@hotmail.com>
2021-04-14 14:35:36 -0300

    update ValidateRegistry

--- middlewares/ValidateRegistry.js
+++ middlewares/ValidateRegistry.js
@@ -3,13 +3,17 @@ const { Usuario } = require('../models');
 module.exports = async (request, response, next) => {
   const { nome, email, senha } = request.body;
 
+  if (!email) {
+    return response.status(400).json({ error: 'Invalid email' });
+  }
+
   const userExists = await Usuario.findAll({
     where: {
       email,
     },
   });
 
-  if (!email || userExists.length) {
+  if (userExists.length) {
     return response.status(404).json({ error: 'Email already registred' });
   } else if (!senha || senha.length < 6 || senha.length > 12) {
     return response.status(400).json({ error: 'Invalid password format' });
